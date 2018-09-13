c1376b234fcfaa126e1ccd2b67aa0560e38f81a7
yousifHmada <yousifhmada.moos@gmail.com>
2018-09-13 07:40:01 +0200

    Initialization

---
+++ Assets/FlexibleUI/FlexibleUiData.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName="FlexibleUIData",menuName="FLexibleUI/Datafile")]
+public class FlexibleUIData : ScriptableObject {
+
+}
