5207495f2d4bfa94643179e7b990854317b5a717
yousifHmada <yousifhmada.moos@gmail.com>
2018-09-13 08:04:53 +0200

    Adding BaseClass

---
+++ Assets/FlexibleUI/FlexibleUiButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(Button))]
+public class FlexibleUiButton : FlexibleUI {
+	Image image;
+	Button button;
+
+	public override void Awake(){
+		base.Awake();
+		image = GetComponent<Image>();
+		button = GetComponent<Button>();
+		Debug.Log("lol");
+	}
+}
--- Assets/FlexibleUI/FlexibleUiData.cs
+++ Assets/FlexibleUI/FlexibleUiData.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 [CreateAssetMenu(fileName="FlexibleUIData",menuName="FLexibleUI/Datafile")]
 public class FlexibleUIData : ScriptableObject {
-
+    public Sprite buttonSprite;
+    public SpriteState buttonSpriteState;
 }
